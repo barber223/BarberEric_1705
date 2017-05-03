4f8c621ae3e99c9ac9291bde69878b92f4b914f6
Eric Barber <eabarber@fullsail.edu>
2017-05-02 22:41:58 -0400

    I am still unable to transfer the information within the columns

--- BarberEric_CE01/Form1.Designer.cs
+++ BarberEric_CE01/Form1.Designer.cs
@@ -121,6 +121,7 @@
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
             // 
             // needCol
--- BarberEric_CE01/Form1.cs
+++ BarberEric_CE01/Form1.cs
@@ -12,7 +12,9 @@ namespace BarberEric_CE01
 {
     public partial class GroceryList : Form
     {
-        string item;
+        int index = -1;
+     
+
 
         public GroceryList ( )
         {
@@ -21,11 +23,11 @@ namespace BarberEric_CE01
 
         private void btnAdd_Click ( object sender,EventArgs e )
         {
-            item = txtItem.Text;
+            
 
             ListViewItem/*.ListViewSubItem*/ it = new ListViewItem ( );//.ListViewSubItem ( );
 
-            it.Text = item;
+            it.Text = txtItem.Text;
 
             listView1.Items.Add (  it);//.SubItems.Add ( item );
 
@@ -39,12 +41,53 @@ namespace BarberEric_CE01
 
         private void btnMove_Click ( object sender,EventArgs e )
         {
+            if (index != -1)
+            {
+                /*
+                if (listView1.Items[0].SubItems.Contains ( listView1.Items[0].SubItems[index] ))
+                {
 
-        }
+                    for (int i = 0; i < listView1.Items[0].SubItems.Count; i++)
+                    {
+                        if (listView1.Items[0].SubItems[index] == listView1.Items[0].SubItems[i])
+                        {
+
+                            MessageBox.Show ( listView1.Items[0].SubItems[index].Text );
+
+                            listView1.Items[1].SubItems.Add ( listView1.Items[0].SubItems[index] );
+                            listView1.Items[0].SubItems.Remove ( listView1.Items[0].SubItems[index] );
+
+
+                            //MessageBox.Show ( "did it recognize" );
+                        }
+
+                    }
+                    */
+
+
+                }
+
+                else if (listView1.Items[1].SubItems.Contains ( listView1.Items[1].SubItems[index] ))
+                {
+
+                }
+
+
+
+                //MessageBox.Show ( "working" );
+                index = -1;
+            }
+        
 
         private void listView1_MouseDoubleClick ( object sender,MouseEventArgs e )
         {
+            index = listView1.Items.IndexOf ( listView1.SelectedItems[0]);
             
+        }
+
+        private void listView1_MouseClick ( object sender,MouseEventArgs e )
+        {
+            index = listView1.Items.IndexOf ( listView1.SelectedItems[0] );
         }
     }
 }
